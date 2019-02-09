87b3d6db2095900b1d732e4e9cbc31dc4f93dd1e
Saqib Ali <saqibali@Saqibs-MacBook-Pro.local>
2019-02-09 10:51:36 +0800

    first commit

---
+++ 01hello/cmake-build-debug/CMakeFiles/01hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01hello.dir/main.c.o"
+  "01hello.pdb"
+  "01hello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/01hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
